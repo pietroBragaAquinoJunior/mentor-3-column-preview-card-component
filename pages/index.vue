fea2bdd4fba8d9342c9301a93e30d4098adf8255
Pietro Braga <pietrobragaprofissional@gmail.com>
2024-10-18 15:54:38 -0300

    finalizado

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="min-h-[100vh] flex justify-center items-center">
+    <div class="flex flex-col md:flex-row w-[330px] h-[1500px] pt-[100px] pb-[100px] md:h-[700px] md:w-[900px]">
+      <div class="bg-[hsl(31,77%,52%)] w-full flex-1 rounded-t-md flex flex-col justify-around p-12 md:rounded-l-md md:rounded-tr-none">
+        <img class="w-[60px]" src="/images/icon-sedans.svg" alt="Icon Sedans">
+        <h1 style="font-family: BigShouldersDisplay;" class="uppercase text-[40px] font-[700] mb-[-40px]">
+          Sedans
+        </h1>
+        <p class="font-[400] leading-6 text-[hsla(0,0%,100%,0.75)]">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city
+          or on your next road trip.
+        </p>
+        <button class="rounded-full p-3 bg-white text-[hsl(31,77%,52%)] w-[140px]">
+          Learn More
+        </button>
+      </div>
+
+      <div class="bg-[hsl(184,100%,22%)] w-full flex-1 flex flex-col justify-around p-12">
+        <img class="w-[60px]" src="/images/icon-suvs.svg" alt="Icon Sedans">
+        <h1 style="font-family: BigShouldersDisplay;" class="uppercase text-[40px] font-[700] mb-[-40px]">
+          suvs
+        </h1>
+        <p class="font-[400] leading-6 text-[hsla(0,0%,100%,0.75)]">
+          Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation
+          and off-road adventures.
+        </p>
+        <button class="rounded-full p-3 bg-white text-[hsl(184,100%,22%)] w-[140px]">
+          Learn More
+        </button>
+      </div>
+
+      <div class="bg-[hsl(179,100%,13%)] w-full flex-1 rounded-b-md flex flex-col justify-around p-12 md:rounded-r-md md:rounded-bl-none">
+        <img class="w-[60px]" src="/images/icon-luxury.svg" alt="Icon Sedans">
+        <h1 style="font-family: BigShouldersDisplay;" class="uppercase text-[40px] font-[700] mb-[-40px]">
+          luxury
+        </h1>
+        <p class="font-[400] leading-6 text-[hsla(0,0%,100%,0.75)]">
+          Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
+          rental and arrive in style.
+        </p>
+        <button class="rounded-full p-3 bg-white text-[hsl(179,100%,13%)] w-[140px]">
+          Learn More
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+  <!--
+
+  Luxury
+  Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
+  rental and arrive in style. -->
